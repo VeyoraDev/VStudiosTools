663eba88bc23d640367041b1affeec51c6d7db8b
VeyoraDev <hafiziedede@gmail.com>
2026-07-31 00:14:29 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,28 +1,18 @@
-// API Router - Main entry point
 export default async function handler(req, res) {
     const { path } = req.query;
     
-    // Enable CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
     
-    // Route to appropriate handler
     try {
         if (path === 'tiktok') {
             const tiktokHandler = await import('./tiktok.js');
             return tiktokHandler.default(req, res);
         }
-        
-        // Add more tools here
-        // if (path === 'youtube') {
-        //     const youtubeHandler = await import('./youtube.js');
-        //     return youtubeHandler.default(req, res);
-        // }
         
         return res.status(404).json({
             status: false,
